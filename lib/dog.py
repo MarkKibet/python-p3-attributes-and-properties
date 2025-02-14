012609dbde28494737704880815c2efb1071b72a
MarkKibet <kemboimarkbiwott@gmail.com>
2025-02-14 11:15:57 +0300

    Attributes and properties

--- lib/dog.py
+++ lib/dog.py
@@ -1,15 +1,38 @@
-#!/usr/bin/env python3
+# lib/dog.py
 
-APPROVED_BREEDS = [
-    "Mastiff",
-    "Chihuahua",
-    "Corgi",
-    "Shar Pei",
-    "Beagle",
-    "French Bulldog",
-    "Pug",
-    "Pointer"
-]
+approved_breeds = ["Mastiff", "Chihuahua", "Corgi", "Shar Pei", "Beagle", "French Bulldog", "Pug", "Pointer"]
 
 class Dog:
-    pass
+    def __init__(self, name="Unknown", breed="Unknown"):
+        self._name = None  
+        self._breed = None
+        if self._validate_name(name):
+            self.name = name
+            self.breed = breed  
+
+    def _validate_name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <= 25:
+            return True
+        else:
+            print("Name must be string between 1 and 25 characters.")
+            return False
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if self._validate_name(value):
+            self._name = value
+
+    @property
+    def breed(self):
+        return self._breed
+
+    @breed.setter
+    def breed(self, value):
+        if value in approved_breeds:
+            self._breed = value
+        else:
+            print("Breed must be in list of approved breeds.")
